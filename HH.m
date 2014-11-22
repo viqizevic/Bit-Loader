16af49657b9c14156032c830981d512aff5921eb
Vicky Tanzil <vicky.tanzil@gmail.com>
2014-11-22 01:26:31 +0100

    Complete HH.m

--- HH.m
+++ HH.m
@@ -1,23 +1,36 @@
+% Author: Vicky Tanzil (vicky.tanzil@gmail.com)
+% Created: 21.11.2014
+%
+% Function: [En, bn] = HH(nlev,pBudget,totBR)
+%
+% Hughes-Hartoggs’s Method
+% as described in the lecture of Dr. Volker Jungnickel at TU Berlin
+%
+% Input:
+% _ nlev is a vector containing the noise levels
+% _ pBudget is the available power budget
+% _ totBR is the total bit rate we try to achieve
+%
+% Output:
+% _ En is the energy in the n-th sub-channel (PAM or QAM)
+% _ bn is the bit in the n-th sub-channel
+%
+function [En, bn] = HH(nlev,pBudget,totBR)
 
 % The maximum number of usable carriers
 n = length(nlev);
 
-function snr = LTSItable(bits)
-  snr = 3*bits + 3;
-end
-
-
 % Initializations
 En = zeros(n,1);
 budget = pBudget;
-target = 8;
-tb = zeros(n,1); % transferred bits in each channel
+target = totBR;
+bn = zeros(n,1); % transferred bits in each channel
 transfer = 0; % total transferred bits
 
 while (transfer < target)
   % get the channel with minimal required power
   % if one more bit added
-  snr = 10.^(LTSItable(tb + 1)/10);
+  snr = 10.^(LTSItable(bn + 1)/10);
   reqEn = nlev .* snr;
   [newEn,idx] = min(reqEn);
   
@@ -30,16 +43,24 @@ while (transfer < target)
   budget = update;
 
   % update bits transferred
-  tb(idx) = tb(idx) + 1;
+  bn(idx) = bn(idx) + 1;
   transfer = transfer + 1;
   En(idx) = newEn;
 end
 
-usedBudget = pBudget - budget;
+end
 
 
-
-
-
-
+% Link to system interface table
+%  #bits   SNR (dB)
+%    1       6
+%    2       9
+%    3      12
+%    4      15
+% To simplify this we use this function
+% SNR = 3*(#bits) + 3
+%
+function snr = LTSItable(bits)
+  snr = 3*bits + 3;
+end
 
--- Test.m
+++ Test.m
@@ -6,8 +6,12 @@
 
 nlev = [1/19.94 1/17.03 1/17.03 1/10.0 1/10.0 1/2.968 1/2.968 1/0.0552]';
 pBudget = 8;
+totBR = 8;
 
 En = WF(nlev,pBudget);
 
 
 lvlWF = En+nlev
+
+
+[En, bn] = HH(nlev,pBudget,totBR)
